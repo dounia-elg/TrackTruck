520f2cbe126ef849ea6746a77c45a793fab8af9d
dounia-elg <dounia.elg20@gmail.com>
2025-12-09 15:18:51 +0100

    feat: initialize backend server with Express framework

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('TrackTruck is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
