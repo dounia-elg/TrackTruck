cea3a919fcc9329d3c6f656f00adb4606f2a45b9
dounia-elg <dounia.elg20@gmail.com>
2025-12-10 14:04:24 +0100

    feat: TRC-3 implement token generation function in authController

---
+++ backend/controllers/authController.js
@@ -0,0 +1,10 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId, userRole) => {
+  return jwt.sign(
+    { id: userId, role: userRole },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+};
