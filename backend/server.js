f9bf830507673ea3bdbb5bd3014d4f121a73423b
dounia-elg <dounia.elg20@gmail.com>
2025-12-10 11:00:10 +0100

    feat: add MongoDB connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 const app = express();
 
 app.use(cors());
+dotenv.config();
 app.use(express.json());
 
 
