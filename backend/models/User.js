5921f98b4b1c2fbc2dcce301d3b9ae5a01096ab9
dounia-elg <dounia.elg20@gmail.com>
2025-12-10 11:56:48 +0100

    feat: TRC-3 create User model with schema definition

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "driver"],
+      default: "driver",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
