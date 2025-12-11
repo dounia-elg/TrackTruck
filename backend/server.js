5c284df04eaabe2866148500dee2c1ba75e5d9eb
dounia-elg <dounia.elg20@gmail.com>
2025-12-11 06:12:21 +0100

    feat: TRC-4 add error handling middleware to server for improved error management

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("TrackTruck is running");
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT  || 3002;
 app.listen(PORT, () => {
