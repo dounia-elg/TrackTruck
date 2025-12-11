4c6678b14a4c579cd997414be8d5254b9295fef5
dounia-elg <dounia.elg20@gmail.com>
2025-12-11 06:14:56 +0100

    feat: TRC-4 add admin and driver routes with role-based access control

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,29 @@ import express from "express";
 import { register, login } from "../controllers/authController.js";
 import { validateRegister, validateLogin } from "../validators/authValidator.js";
 import { validateRequest } from "../middleware/validateRequest.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+import { checkRole } from "../middleware/checkRole.js";
 
 const router = express.Router();
 
 router.post("/register", validateRegister, validateRequest, register);
 router.post("/login", validateLogin, validateRequest, login);
+
+
+router.get("/admin-only", verifyToken, checkRole("admin"), (req, res) => {
+  res.json({ 
+    message: "Welcome Admin!", 
+    user: req.user 
+  });
+});
+
+router.get("/driver-only", verifyToken, checkRole("driver"), (req, res) => {
+  res.json({ 
+    message: "Welcome Driver!", 
+    user: req.user 
+  });
+});
+
 
 
 export default router;
